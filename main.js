ab57826a32c82b25c24fd04e45dce0b22c7c71f3
asoolito <roblox890king890@gmail.com>
2024-06-12 11:55:24 +0500

    50y5

---
+++ main.js
@@ -0,0 +1,6 @@
+let userName = prompt("so'z kiriting")
+text = document.createElement("h1")
+text.textContent = userName
+text.style.color = "yellow"
+text.style.fontSize= "100px"
+document.body.appendChild(text)
